1c877c8b1e0615a34fb93c1aa6a2a35e6d64e648
Rishi Khurana <league@WTS-IM-17.attlocal.net>
2017-10-13 18:28:29 -0700

    Unbirthday (IP)

---
+++ src/Unbirthday.java
@@ -0,0 +1,7 @@
+import javax.swing.JOptionPane;
+
+public class Unbirthday {
+	public static void main(String[] args) {
+		JOptionPane.showConfirmDialog(null, "What is your birthday? mm/dd");
+	}
+}
